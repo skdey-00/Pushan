0aba04fca28cf1b967ac9e9a457075a1a7e77be4
skdey-00 <sanmeetdey@gmail.com>
2026-03-29 11:12:43 +0530

    Add traffic controller launcher script

---
+++ run_traffic_controller.bat
@@ -0,0 +1,9 @@
+@echo off
+echo ============================================================
+echo Starting Traffic Controller...
+echo ============================================================
+echo.
+
+"C:\Users\sanme\AppData\Local\Programs\Python\Python311\python.exe" "%~dp0traffic_controller.py"
+
+pause
